d32b33fcdbd56bd36769812ef81d9ef0c2b6a724
Darcy <32188044+Darcman99@users.noreply.github.com>
2020-08-19 15:58:50 +0930

    Add robot battery size research

--- prototypes/technology.lua
+++ prototypes/technology.lua
@@ -30,4 +30,126 @@ data:extend({
 		upgrade=true,
 		order="c-k-f-e"
 	},
+
+	{
+		type = "technology",
+		name = "worker-robot-battery-1",
+		icon = "__Infinite_Technology__/worker-robot-battery.png",
+		icon_size = 128,
+		effects =
+		{
+			{
+				type = "worker-robot-battery",
+				modifier = 0.2
+			}
+		},
+		prerequisites = {"robotics"},
+		unit =
+		{
+			count_formula = "100*L",
+			ingredients =
+			{
+				{"automation-science-pack", 1},
+				{"logistic-science-pack", 1},
+				{"chemical-science-pack", 1},
+			},
+			time = 30
+		},
+		upgrade = true,
+		max_level = "3",
+		order = "c-k-h-e"
+	},
+	
+	{
+		type = "technology",
+		name = "worker-robot-battery-4",
+		icon = "__Infinite_Technology__/worker-robot-battery.png",
+		icon_size = 128,
+		effects =
+		{
+			{
+				type = "worker-robot-battery",
+				modifier = 0.2
+			}
+		},
+		prerequisites = {"worker-robot-battery-1"},
+		unit =
+		{
+			count_formula = "100*L",
+			ingredients =
+			{
+				{"automation-science-pack", 1},
+				{"logistic-science-pack", 1},
+				{"chemical-science-pack", 1},
+				{"production-science-pack", 1},
+			},
+			time = 45
+		},
+		upgrade = true,
+		max_level = "7",
+		order = "c-k-h-e"
+	},
+
+	{
+		type = "technology",
+		name = "worker-robot-battery-8",
+		icon = "__Infinite_Technology__/worker-robot-battery.png",
+		icon_size = 128,
+		effects =
+		{
+			{
+				type = "worker-robot-battery",
+				modifier = 0.2
+			}
+		},
+		prerequisites = {"worker-robot-battery-4"},
+		unit =
+		{
+			count_formula = "100*L",
+			ingredients =
+			{
+				{"automation-science-pack", 1},
+				{"logistic-science-pack", 1},
+				{"chemical-science-pack", 1},
+				{"production-science-pack", 1},
+				{"utility-science-pack", 1},
+			},
+			time = 60
+		},
+		upgrade = true,
+		max_level = "11",
+		order = "c-k-h-e"
+	},
+
+	{
+		type = "technology",
+		name = "worker-robot-battery-12",
+		icon = "__Infinite_Technology__/worker-robot-battery.png",
+		icon_size = 128,
+		effects =
+		{
+			{
+				type = "worker-robot-battery",
+				modifier = 0.2
+			}
+		},
+		prerequisites = {"worker-robot-battery-8"},
+		unit =
+		{
+			count_formula = "100*L",
+			ingredients =
+			{
+				{"automation-science-pack", 1},
+				{"logistic-science-pack", 1},
+				{"chemical-science-pack", 1},
+				{"production-science-pack", 1},
+				{"utility-science-pack", 1},
+				{"space-science-pack", 1},
+			},
+			time = 60
+		},
+		upgrade = true,
+		max_level = "infinite",
+		order = "c-k-h-e"
+	},
 })
